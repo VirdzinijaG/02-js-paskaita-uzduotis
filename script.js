a088a59983beba27aff964ec2b741fb051c309c1
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-06-09 10:04:04 +0300

    Div

--- script.js
+++ script.js
@@ -25,14 +25,17 @@ console.log(skaicius1, skaicius2);
 
 // Sukeisti skaicius3 ir skaicius4 reikšmes vietomis be laikinojo kintamojo.
 
+skaicius3 = skaicius3 + skaicius4; // 6+10=16
+skaicius4 = skaicius3 - skaicius4; // 16-10=6
+skaicius3 = skaicius3 - skaicius4; // 16-6=10
+
+console.log(skaicius3, skaicius4);
+
 // Į alert išvesti sukeitimo rezultatus.
 
-alert("skaicius1 " + skaicius1 + ' skaicius2 ' + skaicius2 );
+alert("skaicius1 " + skaicius1 + ' skaicius2 ' + skaicius2 + ' skaicius3 ' + skaicius3 + ' skaicius4 ' + skaicius4);
 
 // Susikurti kintamąjį maxSkaicius, jam priskirti maksimalią įmanomą skaitinę reikšmę (infinity netinka) ir išvesti į konsolę.
-
-var maxSkaicius
-maxSkaicius = skaicius1, skaicius2, skaicius3, skaicius4;
 
 console.log(Math.max(skaicius1, skaicius2, skaicius3, skaicius4));
 
@@ -41,8 +44,16 @@ var skaicius5, skaicius6;
 skaicius5 = 15;
 skaicius6 = 20;
 
+// Sukeisti kinamųju reikšmes skaicius5 ir skaicius6 pasitelkiant XOR operatorių.
+
+skaicius5 = skaicius5 ^ skaicius6;
+skaicius6 = skaicius5 ^ skaicius6;
+skaicius5 = skaicius5 ^ skaicius6;
+
 console.log(skaicius5, skaicius6);
 
-// Sukeisti kinamųju reikšmes skaicius5 ir skaicius6 pasitelkiant XOR operatorių.
+// Rezultatą patalpinti į HTML dokumento div žymėje, kurios id="Rezultatas".
+
+document.getElementById("rezultatas").innerHTML += 'skaicius5 = ' + skaicius5 + ' skaicius6 = ' + skaicius6;
 
 
